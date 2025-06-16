de2756f84fff00203b36c4005b3b683dc899ec46
Adun Nantakaew <amphancm@gmail.com>
2025-06-16 10:52:53 +0700

    update train2

--- olmocr/train/utils.py
+++ olmocr/train/utils.py
@@ -55,7 +55,9 @@ def make_dataset(config: TrainConfig, processor: AutoProcessor) -> tuple[Dataset
     elif "allenai" in config.model.name_or_path.lower():
         batch_fn = batch_prepare_data_for_qwen2_training
     elif "scb10x" in config.model.name_or_path.lower():
-        batch_fn = batch_prepare_data_for_qwen2_training        
+        batch_fn = batch_prepare_data_for_qwen2_training
+    elif "Adun" in config.model.name_or_path.lower():
+        batch_fn = batch_prepare_data_for_qwen2_training           
     elif "molmo" in config.model.name_or_path.lower():
         batch_fn = batch_prepare_data_for_molmo_training
     else:
